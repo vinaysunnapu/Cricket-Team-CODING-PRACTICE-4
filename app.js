2b4d06b1f92cd46304168d68cc9fc4b26cd83612
vinaysunnapu <vinaysunnapu@gmail.com>
2022-11-09 07:49:14 +0000

    Cricket Team CODING PRACTICE 4

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const { open } = require("sqlite");
 const sqlite3 = require("sqlite3");
 const path = require("path");
 const app = express();
+app.use(express.json());
 
 const dbPath = path.join(__dirname, "cricketTeam.db");
 
@@ -20,6 +21,7 @@ const initializeDBAndServer = async () => {
     process.exit(1);
   }
 };
+module.exports = app;
 
 initializeDBAndServer();
 
@@ -30,4 +32,23 @@ app.get("/players/", async (request, response) => {
   const playersArray = await db.all(getPlayersQuery);
   response.send(playersArray);
 });
-module.exports = app;
+
+//API2
+
+app.post("/players/", async (request, response) => {
+  const playerDetails = request.body;
+  const { player_id, playerName, jerseyNumber, role } = playerDetails;
+  const addPlayerQuery = `
+   INSERT INTO 
+   cricket_team( playerName, jerseyNumber, role)
+   VALUES(
+       
+      '${playerName}',
+       ${jersyNumber},
+       '${role}'
+   );
+  `;
+  const dbResponse = await db.run(addPlayerQuery);
+  const playerId = dbResponse.lastID;
+  response.send({ player_id: playerId });
+});
